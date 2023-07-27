7c18e51f35359a00c202d8775195f373706ce4ec
Filip <fpodsiadly@gmail.com>
2023-07-27 18:29:56 +0200

    add updatedAt

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -8,11 +8,13 @@ renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
   const id = uuidv4();
-
+  const timestamp = moment().valueOf();
   notes.push({
     id: id,
     title: '',
     body: '',
+    createdAt: timestamp,
+    updatedAt: timestamp
   });
   saveNotes(notes);
   location.assign(`edit.html#${id}`);

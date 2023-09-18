1b85e37b51146fc5542537d960742d75e1b9266d
Filip <fpodsiadly@gmail.com>
2023-09-18 16:12:48 +0200

    subclasses

--- oop/person.js
+++ oop/person.js
@@ -22,9 +22,42 @@ class Person {
     }
 }
 
-const me = new Person('Filip', 'P', 33, ['Teaching', 'Biking']);
-me.setName('Alexis Turner')
-console.log(me.getBio());
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}.`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
+}
 
-const person2 = new Person('Clancey', 'Turner', 50)
-console.log(person2.getBio());
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+        this.grade += change
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
+}
+
+const me = new Student('Filip', 'P', 33, 88, [])
+console.log(me.getBio())
+me.updateGrade(-20)
+console.log(me.getBio())
+
+//const me = new Employee('Filip', 'P', 33, 'Engineer', ['Teaching', 'Biking']);
+// me.setName('Alexis Turner')
+// console.log(me.getBio());
+// console.log(me.getYearsLeft())
+
+// const person2 = new Person('Clancey', 'Turner', 50)
+// console.log(person2.getBio());

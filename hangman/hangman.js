c1b06ee5dd368acd3ecab441c71688673c8737f6
Filip <fpodsiadly@gmail.com>
2023-09-18 12:56:59 +0200

    hangman part 4 ,status message

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -35,6 +35,16 @@ Hangman.prototype.calculateStatus = function () {
     }
 }
 
+Hangman.prototype.getStatusMessage = function () {
+    if (this.status === 'playing') {
+        return `Guesses left: ${this.remainingGuesses}`
+    } else if (this.status === 'failed') {
+        return `Nice try! The word was "${this.word.join('')}".`
+    } else {
+        return 'Great work! You guessed the work.'
+    }
+}
+
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
 
@@ -54,6 +64,12 @@ Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
+
+    if (this.status !== 'playing') {
+        return
+    }
+
+
     if (isUnique) {
         this.guessedLetters.push(guess)
     }

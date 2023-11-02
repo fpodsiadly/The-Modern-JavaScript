46895baa44dfb7e34a39fb0860a7797267ef2abd
Filip <fpodsiadly@gmail.com>
2023-11-02 18:43:42 +0100

    getters and setters and small bug fix

--- oop/person.js
+++ oop/person.js
@@ -1,63 +1,57 @@
 // Prototype Inheritance
 
 class Person {
-    constructor(firstName, lastName, age, likes = []) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.likes = likes
-    }
-    getBio() {
-        let bio = `${this.firstName} is ${this.age}.`
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`
 
-        this.likes.forEach((like) => {
-            bio += ` ${this.firstName} lik es ${like}.`
-        })
-        return bio
-    }
-    setName(fullName) {
-        const names = fullName.split(' ')
-        this.firstName = names[0]
-        this.lastName = names[1]
-    }
+    this.likes.forEach((like) => {
+      bio += ` ${this.firstName} lik es ${like}.`
+    })
+    return bio
+  }
+  set fullName(fullName) {
+    const names = fullName.split(" ")
+    this.firstName = names[0]
+    this.lastName = names[1]
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
 }
 
 class Employee extends Person {
-    constructor(firstName, lastName, age, position, likes) {
-        super(firstName, lastName, age, likes)
-        this.position = position
-    }
-    getBio() {
-        return `${this.firstName} ${this.lastName} is a ${this.position}.`
-    }
-    getYearsLeft() {
-        return 65 - this.age
-    }
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio() {
+    return `${this.fullName} is a ${this.position}.`
+  }
+  getYearsLeft() {
+    return 65 - this.age
+  }
 }
 
 class Student extends Person {
-    constructor(firstName, lastName, age, grade, likes) {
-        super(firstName, lastName, age, likes)
-        this.grade = grade
-    }
-    updateGrade(change) {
-        this.grade += change
-    }
-    getBio() {
-        const status = this.grade >= 70 ? 'passing' : 'failing'
-        return `${this.firstName} is ${status} the class.`
-    }
+  constructor(firstName, lastName, age, grade, likes) {
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+  }
+  updateGrade(change) {
+    this.grade += change
+  }
+  getBio() {
+    const status = this.grade >= 70 ? "passing" : "failing"
+    return `${this.firstName} is ${status} the class.`
+  }
 }
 
-const me = new Student('Filip', 'P', 33, 88, [])
+const me = new Employee("Filip", "P", 33, "Teacher", [])
+me.fullName = "Filip P"
 console.log(me.getBio())
-me.updateGrade(-20)
-console.log(me.getBio())
-
-//const me = new Employee('Filip', 'P', 33, 'Engineer', ['Teaching', 'Biking']);
-// me.setName('Alexis Turner')
-// console.log(me.getBio());
-// console.log(me.getYearsLeft())
-
-// const person2 = new Person('Clancey', 'Turner', 50)
-// console.log(person2.getBio());

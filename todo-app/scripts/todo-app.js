803112108764e7ffa3615c59bebc20fb70d439fc
Filip <fpodsiadly@gmail.com>
2023-11-07 16:10:53 +0100

    fix todo app styling and code

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -3,19 +3,14 @@
 let todos = getSavedTodos()
 
 const filters = {
-  searchTodo: "",
+  searchText: "",
   hideCompleted: false,
 }
 
 renderTodos(todos, filters)
 
-// const incompleteTodos = todos.filter(function (todo) {
-//   return !todo.completed;
-// });
-
-//listen for todo input
 document.querySelector("#search-text").addEventListener("input", (e) => {
-  filters.searchTodo = e.target.value
+  filters.searchText = e.target.value
   renderTodos(todos, filters)
 })
 
@@ -23,7 +18,7 @@ document.querySelector("#new-todo").addEventListener("submit", (e) => {
   const text = e.target.elements.text.value.trim()
   e.preventDefault()
 
-  if (text.lenght > 0) {
+  if (text.length > 0) {
     todos.push({
       id: uuidv4(),
       text,
@@ -35,7 +30,7 @@ document.querySelector("#new-todo").addEventListener("submit", (e) => {
   }
 })
 
-document.querySelector("#hide-complited").addEventListener("change", (e) => {
+document.querySelector("#hide-completed").addEventListener("change", (e) => {
   filters.hideCompleted = e.target.checked
   renderTodos(todos, filters)
 })

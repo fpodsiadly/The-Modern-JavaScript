b983134d37cbefb7d7555c9529e6fb0ec3701be5
Filip <fpodsiadly@gmail.com>
2023-11-15 19:01:56 +0100

    rest

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,10 +1,23 @@
-//import "./utilities"
-import otherSquare, { add, name } from "./utilities"
-import otherScream from "./scream"
+const calculateAverage = (thing, ...numbers) => {
+  //return (numOne + numTwo) / 2
+  let sum = 0
+  numbers.forEach((num) => (sum += num))
+  const average = sum / numbers.length
+  return `The average ${thing} is ${average}`
+}
 
-console.log("index.js")
-console.log(add(32, 1))
-console.log(name)
-console.log(otherScream(name))
-console.log(otherSquare(10))
-console.log("last test")
+console.log(calculateAverage('age', 0, 10, 88, 64))
+
+// Create printTeam that takes team name, coach, and players
+
+// Team: Liberty
+// Coach: Casey Penn
+// Players: Marge, Aiden, Herbert, Sherry
+
+const printTeam = (teamName, coach, ...players) => {
+  console.log(`Team: ${teamName}`)
+  console.log(`Coach: ${coach}`)
+  console.log(`Players: ${players.join(', ')}`)
+}
+
+printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')

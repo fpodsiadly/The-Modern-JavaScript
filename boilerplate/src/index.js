77474ed58a05678c9077ccc7a05f207d71999ba4
Filip <fpodsiadly@gmail.com>
2023-11-15 19:39:33 +0100

    spread objects

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -15,3 +15,38 @@ let cities = ['London', 'Paris', 'New York']
 cities = ['Santiago', ...cities]
 
 console.log(cities)
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1,
+  yearBuild: 2000,
+}
+
+let newHouse = {
+  basement: true,
+  ...house,
+  bedrooms: 3,
+}
+
+newHouse.yearBuild = 2003
+
+console.log(house)
+console.log(newHouse)
+
+const person = {
+  name: 'Casey',
+  age: 30,
+}
+
+const location = {
+  city: 'New York',
+  country: 'USA',
+}
+
+const oveview = {
+  ...person,
+  job: 'Software Engineer',
+  ...location,
+}
+
+console.log(oveview)
